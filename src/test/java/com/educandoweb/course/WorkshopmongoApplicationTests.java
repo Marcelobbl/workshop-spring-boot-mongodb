b635272c3defeb9523be79a16ad73e4cce3537ea
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-29 20:25:11 -0300

    Usando padrão dto para retornar usuarios

--- src/test/java/com/educandoweb/course/WorkshopmongoApplicationTests.java
+++ src/test/java/com/educandoweb/course/WorkshopmongoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class WorkshopmongoApplicationTests {
+class WorkshopmongoApplicationTests {
 
 	@Test
 	void contextLoads() {
