05c4e42006b1df845e9e24b47a1a5ec31956b0dd
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-29 19:32:36 -0300

    Operacao de instanciação da base de dados

--- src/test/java/com/educandoweb/course/WorkshopmongoApplicationTests.java
+++ src/test/java/com/educandoweb/course/WorkshopmongoApplicationTests.java
@@ -1,10 +1,10 @@
 package com.educandoweb.course;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WorkshopmongoApplicationTests {
+public class WorkshopmongoApplicationTests {
 
 	@Test
 	void contextLoads() {
