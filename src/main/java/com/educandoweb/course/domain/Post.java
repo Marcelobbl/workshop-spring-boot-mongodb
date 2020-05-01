85e8d7f5add567b0682e7b618d82ac07d69e2553
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-30 21:08:05 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/educandoweb/course/domain/Post.java
+++ src/main/java/com/educandoweb/course/domain/Post.java
@@ -1,17 +1,20 @@
 package com.educandoweb.course.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.educandoweb.course.dto.AuthorDTO;
+import com.educandoweb.course.dto.CommentDTO;
 
 @Document
-public class Post implements Serializable{
+public class Post implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	private String id;
 	private Date date;
@@ -19,8 +22,10 @@ public class Post implements Serializable{
 	private String body;
 	private AuthorDTO author;
 
+	private List<CommentDTO> comments = new ArrayList<>();
+
 	public Post() {
-		}
+	}
 
 	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
@@ -70,7 +75,15 @@ public class Post implements Serializable{
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
-	
+
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -95,6 +108,5 @@ public class Post implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
+
 }
